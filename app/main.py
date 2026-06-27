0fbfb2eb55959fc9015d2fe5093df2f8bbc95491
Varun N Yaligar <varunyaligar1@gmail.com>
2026-06-27 16:54:40 +0530

    test git

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from routers import company,job
 from database import Base,engine,SessionLocal
+from models import company as company_model, job as job_model
 
 app = FastAPI()
 print("engine",engine)
