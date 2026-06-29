6b66cea2879a102519a7b844adeb700b05b9c1ac
Varun N Yaligar <varunyaligar1@gmail.com>
2026-06-29 12:09:22 +0530

    test git

--- alembic/env.py
+++ alembic/env.py
@@ -2,7 +2,6 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -18,7 +17,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from database import Base
+from models.company import Company
+from models.job import Job
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
