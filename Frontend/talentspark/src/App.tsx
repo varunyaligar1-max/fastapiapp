51fafeee957c6faf8a5ca63f272af28679444fe6
Varun N Yaligar <varunyaligar1@gmail.com>
2026-06-29 16:44:43 +0530

    test git

--- Frontend/talentspark/src/App.tsx
+++ Frontend/talentspark/src/App.tsx
@@ -1,11 +1,13 @@
 import Welcome from './components/Welcome';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
+import JobCard from './components/JobCard';
 function App() {
   return (
       <div>
         <Navbar />
         <Welcome />
+        <JobCard />
         <Footer />
       </div>
   );
---
+++ Frontend/talentspark/src/components/JobCard.tsx
@@ -0,0 +1,11 @@
+function JobCard() {
+    return (
+        <div>
+            <h2>Software Engineer</h2>
+            <p>Google</p>
+            <p>Bangalore</p>
+            <p>5LPA</p>
+        </div>
+    );
+}
+export default JobCard;
